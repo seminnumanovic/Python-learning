7d6196e68b94775272f5a979f4bd87d449eb036b
seminnumanovic <semin.contact@gmail.com>
2025-01-26 00:58:31 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+# name = input('What is your name? : ')
+# print('Hello',name + ' ,have a nice day')
+import math
+
+# a = int(input('unesi a '))
+# b = int(input('unesi b '))
+# a *= a
+# b *= b
+# c = math.sqrt(a + b)
+# print(c)
+
+tuple = (1,2,3, 'string')
+list = [1,2,3, 'string']
+set = {1,2,3,4,4}
+Dictionary = {}
+list.append('another word')
+print(set)
