e7b16ac93f1f8eee8cd613b3495c02e1aa985900
seminnumanovic <semin.contact@gmail.com>
2025-01-26 01:18:31 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -9,9 +9,16 @@ import math
 # c = math.sqrt(a + b)
 # print(c)
 
-tuple = (1,2,3, 'string')
-list = [1,2,3, 'string']
-set = {1,2,3,4,4}
-Dictionary = {}
-list.append('another word')
-print(set)
+# tuple = (1,2,3, 'string')
+# list = [1,2,3, 'string']
+# set = {1,2,3,4,4}
+# Dictionary = {'Imena': [54,44,32]}
+# # list.append('another word')
+
+# user_list = ['Lisa', 'Bob', 'Alex', 'Anna', 'John']
+# Dictionary[]
+
+num_list = [1,2,3,4,5,6,7,8,9,10]
+print(num_list[7:0:-2])
+num_list.reverse()
+print(num_list[2:9:2])
